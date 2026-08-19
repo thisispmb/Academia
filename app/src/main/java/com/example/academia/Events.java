376a4331fe19071270a8d5ed25844cfb184124e1
&mp <thisispembe@gmail.com>
2026-08-19 10:08:36 +0300

    Add events fragment

---
+++ app/src/main/java/com/example/academia/Events.java
@@ -0,0 +1,26 @@
+package com.example.academia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Events extends Fragment {
+
+    public Events() {
+        super(R.layout.fragment_events);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+}
