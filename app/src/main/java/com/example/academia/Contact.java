b6ffac4bf2c34cc905c8ba6b693d36d01761c119
&mp <thisispembe@gmail.com>
2026-08-19 10:10:42 +0300

    Add contact fragment

---
+++ app/src/main/java/com/example/academia/Contact.java
@@ -0,0 +1,25 @@
+package com.example.academia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Contact extends Fragment {
+    public Contact() {
+        super(R.layout.fragment_contact);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+}
