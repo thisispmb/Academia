b79ae6a11412a31e323de9da31a164fc6d506593
&mp <thisispembe@gmail.com>
2026-08-19 10:07:02 +0300

    Add department fragment

---
+++ app/src/main/java/com/example/academia/Department.java
@@ -0,0 +1,25 @@
+package com.example.academia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Department extends Fragment {
+    public Department() {
+        super(R.layout.fragment_department);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_department, container, false);
+    }
+}
